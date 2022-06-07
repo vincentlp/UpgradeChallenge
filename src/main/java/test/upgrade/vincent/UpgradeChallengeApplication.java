92a8cc47049fdf04771238d3d0588fdb652dd847
vincentlp <vincent.lepollozec@gmail.com>
2022-06-07 18:41:38 -0400

    first springboot commit

---
+++ src/main/java/test/upgrade/vincent/UpgradeChallengeApplication.java
@@ -0,0 +1,13 @@
+package test.upgrade.vincent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpgradeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpgradeChallengeApplication.class, args);
+	}
+
+}
