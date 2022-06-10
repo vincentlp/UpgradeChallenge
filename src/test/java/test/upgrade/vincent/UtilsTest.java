ab29399b76c6ee19f5cc009fa58b28b0abfeed86
vincentlp <vincent.lepollozec@gmail.com>
2022-06-10 09:03:13 -0400

    add test with concurrent calls

--- src/test/java/test/upgrade/vincent/UtilsTest.java
+++ src/test/java/test/upgrade/vincent/UtilsTest.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import test.upgrade.vincent.controllers.models.CreateReservationDto;
+import test.upgrade.vincent.reservations.models.Reservation;
 
 public final class UtilsTest {
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
@@ -11,11 +12,20 @@ public final class UtilsTest {
     public static CreateReservationDto buildCreateDto(Long offsetArrival, Long offsetDeparture) {
         return CreateReservationDto.builder()
                 .campsiteId("unusedCampsiteId")
-                .name("user1")
-                .email("user1@gmail.com")
+                .name("user")
+                .email("user@gmail.com")
                 .arrivalDate(LocalDate.now().plusDays(offsetArrival).toString())
                 .departureDate(LocalDate.now().plusDays(offsetDeparture).toString())
                 .build();
     }
 
+    public static Reservation buildReservation(LocalDate arrivalDate, LocalDate departureDate){
+        return Reservation.builder()
+                .campsiteId("unusedCamsiteId")
+                .userName("user")
+                .userEmail("user@gmail.com")
+                .startDate(arrivalDate)
+                .endDate(departureDate)
+                .build();
+    }
 }
--- src/test/java/test/upgrade/vincent/reservations/CreateReservationTest.java
+++ src/test/java/test/upgrade/vincent/reservations/CreateReservationTest.java
@@ -2,6 +2,13 @@ package test.upgrade.vincent.reservations;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -14,7 +21,9 @@ import test.upgrade.vincent.UtilsTest;
 import test.upgrade.vincent.controllers.ReservationController;
 import test.upgrade.vincent.controllers.models.CreateReservationDto;
 import test.upgrade.vincent.reservations.models.Reservation;
+import test.upgrade.vincent.reservations.models.ReservationAction;
 import test.upgrade.vincent.reservations.repositories.ReservationRepository;
+import test.upgrade.vincent.workers.ReservationActionService;
 
 
 @SpringBootTest
@@ -26,6 +35,11 @@ public class CreateReservationTest {
 
     @Autowired
     private ReservationRepository reservationRepository;
+
+    @Autowired
+    private ReservationActionService actionService;
+
+    private Reservation defaultReservation;
 
     @BeforeEach
     public void beforeEach() {
@@ -85,6 +99,61 @@ public class CreateReservationTest {
         assertThat(response.getBody()).isNotNull();
         assertThat(response.getBody().toString()).contains("in the past");
 
+    }
+
+    @Test
+    public void with_multithread_on_same_range_date_create_unique_reservation() throws InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(10);
+
+        IntStream.range(0, 1000)
+                .forEach(count -> service.submit(this::simulateNewReservation));
+
+        service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+
+        List<Reservation> allReservationCreated = this.reservationRepository.findAll();
+        assertThat(allReservationCreated.size()).isEqualTo(1);
+
+    }
+
+    @Test
+    public void with_multithread_on_same_range_date_update_unique_reservation() throws InterruptedException {
+
+        defaultReservation = (Reservation) this.reservationController.createReservation(UtilsTest.buildCreateDto(2L, 4L)).getBody();
+        ExecutorService service = Executors.newFixedThreadPool(10);
+
+        IntStream.range(0, 1000)
+                .forEach(count -> {
+                    service.submit(this::simulateUpdateReservation);
+                    if (count == 500) service.submit(this::simulateNewReservation);
+                });
+
+        service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+
+        List<Reservation> allReservationCreated = this.reservationRepository.findAll();
+        assertThat(allReservationCreated.size()).isEqualTo(2);
+
+    }
+
+    private Runnable simulateNewReservation() {
+        LocalDate arrivalDate = LocalDate.now().plusDays(2);
+        LocalDate departureDate = LocalDate.now().plusDays(4);
+        try {
+            this.actionService.performAction(UtilsTest.buildReservation(arrivalDate, departureDate), ReservationAction.CREATE);
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
+
+    private Runnable simulateUpdateReservation() {
+        defaultReservation.setStartDate(defaultReservation.getStartDate().plusDays(7));
+        defaultReservation.setEndDate(defaultReservation.getEndDate().plusDays(7));
+        try {
+            this.actionService.performAction(defaultReservation, ReservationAction.UPDATE);
+        } catch (Exception e) {
+
+        }
+        return null;
     }
 
 }
